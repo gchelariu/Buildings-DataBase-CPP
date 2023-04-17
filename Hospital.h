be2575802c562138c36870d78ebd58d10e0cecdc
MihaiChelariu <127751440+MihaiChelariu@users.noreply.github.com>
2023-04-17 14:13:26 +0300

    Create Hospital.h

---
+++ Hospital.h
@@ -0,0 +1,45 @@
+#include "Building.h"
+
+class Hospital: public Building
+{
+public:
+    Hospital (string name)
+    {
+        mBuildingName = name;
+        mType = "Hospital";
+        mCitizens.clear();
+        mEmployees.clear();
+    }
+
+    Building* operator + (const Person& persoana)
+    {
+        mEmployees.push_back(persoana);
+        return this;
+    }
+
+    Building* operator - (const Person& persoana)
+    {
+        for (int i = 0 ; i < mEmployees.size(); i++)
+            if(mEmployees[i] == persoana)
+            {
+                mEmployees.erase(mEmployees.begin() + i);
+                break;
+            }
+        return this;
+    }
+
+    void add (const Person& persoana)
+    {
+        mCitizens.push_back(persoana);
+    }
+
+    void remove (const Person& persoana)
+    {
+        for (int i = 0 ; i < mCitizens.size(); i++)
+            if(mCitizens[i] == persoana)
+            {
+                mCitizens.erase(mCitizens.begin() + i);
+                break;
+            }
+    }
+};
